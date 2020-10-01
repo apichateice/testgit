a81a074e89d174997bbc260c48d0fba6b732e77e
apichate.ice@gmail.com <ADT059@DESKTOP-VERK2R0.outsource.ktb.co.th>
2020-10-01 15:38:03 +0700

    deploy with tomcat

--- src/main/java/com/example/testingdemo/TestingDemoApplication.java
+++ src/main/java/com/example/testingdemo/TestingDemoApplication.java
@@ -4,15 +4,12 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestingDemoApplication {
+public class TestingDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(TestingDemoApplication.class, args);
-		SpringApplication app = new SpringApplication(TestingDemoApplication.class);
-		app.setDefaultProperties(Collections
-		          .singletonMap("server.port", "8083"));
-		        app.run(args);
+		SpringApplication.run(TestingDemoApplication.class, args);
 	}
 }
